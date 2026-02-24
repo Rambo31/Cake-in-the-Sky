23a7ee08b438d7ea84716a1709334091c2e9de15
Rambo31 <slava-goodkov@mail.ru>
2026-02-24 15:53:47 +0300

    partially finished ArticlesController

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,59 @@
+<?php
+// src/Controller/ArticlesController.php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+class ArticlesController extends AppController
+{
+    public function index(): void
+    {
+        $articles = $this->paginate($this->Articles); //fetch paginated set of Articles
+        $this->set(compact('articles')); //pass articles into the Template
+    }
+
+    public function view(?string $slug): void
+    {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail(); //query
+        $this->set(compact('article'));
+    }
+
+    public function add(): void
+    {
+        $article = $this->Articles->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+
+            // Hardcoding the user_id is temporary, and will be removed later
+            // when we build authentication out.
+            $article->user_id = 1;
+
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article.'));
+        }
+        $this->set('article', $article);
+    }
+
+    public function edit(?string $slug): void
+    {
+        $article = $this->Articles
+                        ->findBySlug($slug)
+                        ->firstOrFail();
+
+        if ($this->request->is(['post', 'put'])) {
+            $this->Articles->patchEntity($article, $this->request->getData());
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been updated.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to update your article.'));
+        }
+
+        $this->set('article', $article);
+    }
+}
